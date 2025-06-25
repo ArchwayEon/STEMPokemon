507f8a198561cd4a6d73b2381013927eb32e213f
Roach, Jeff <ROACHJ@ETSU.EDU>
2025-06-25 10:52:25 -0400

    Updated to 8.0

--- STEMPokemon/Program.cs
+++ STEMPokemon/Program.cs
@@ -27,12 +27,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+//app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
